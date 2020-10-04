23732970e982efeda3f79daaf3e86d9c391da70c
Gockster <gm.dimitriadis@hotmail.com>
2020-10-04 16:22:03 +0300

    Intitial spring commit

--- src/main/java/com/george/springboot/mycrud/dao/EmployeeRepository.java
+++ src/main/java/com/george/springboot/mycrud/dao/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.george.springboot.mycrud.entity.Employee;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
